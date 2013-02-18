61450abd2383f3e01af06619dce99354c97aa268
Martin Poboril <martin.poboril@eagles.oc.edu>
2013-02-18 15:01:33 -0600

    Finish Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,18 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+  
+  describe "Contact page" do
+
+    it "should have the h1 'Contacts'" do
+      visit '/static_pages/contacts'
+      page.should have_selector('h1', :text => 'Contact page')
+    end
+
+    it "should have the title 'Contact page'" do
+      visit '/static_pages/contacts'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact page")
+    end
+  end
 end
